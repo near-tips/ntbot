d5107e788a31975bb5caa19c6b7507b80c100420
hlib <kiskesis@gmail.com>
2021-12-18 04:12:42 +0200

    test

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const axios = require("axios");
-const https = require('https');
+const querystring = require('querystring');
 
 const router = express.Router();
 
 const accessToken = 'GxHLa*A4*eJE6eLelOnIOg))'
+const userId = '17702641'
 
 router.post('/notify', async (req, res) => {
     try {
@@ -18,32 +19,18 @@ router.post('/notify', async (req, res) => {
             }
         })
 
-        console.log('data', data);
+        if (data.items.some(({ owner }) => owner.user_id.toString() === userId)) {
+            res.send('ok')
+        }
 
-        // https.get(`https://api.stackexchange.com/2.3/posts/${postId}/comments/add`, (resp) => {
-        //     let data = '{"site": "stackoverflow", "access_token": "GxHLa*A4*eJE6eLelOnIOg))", "key": "6xURMARqiKBjGkKi0BQJkA((", "body": "Hi, your post was granted with tips, check it out on app.next-tips.com"}';
-        //
-        //     // A chunk of data has been received.
-        //     resp.on('data', (chunk) => {
-        //         data += chunk;
-        //     });
-        //
-        //     // The whole response has been received. Print out the result.
-        //     resp.on('end', () => {
-        //         console.log(JSON.parse(data).explanation);
-        //     });
-        //
-        // }).on("error", (err) => {
-        //     console.log("Error: " + err.message);
-        // });
-
-
-        await axios.post(`https://api.stackexchange.com/2.3/posts/70393641/comments/add`, {
-            body: {
-                site: 'stackoverflow',
-                access_token: accessToken,
-                key: '6xURMARqiKBjGkKi0BQJkA((',
-                body: `Hi, @${username} your post was granted with tips, check it out on app.next-tips.com`
+        await axios.post(`https://api.stackexchange.com/2.3/posts/${postId}/comments/add`, querystring.stringify({
+            site: 'stackoverflow',
+            access_token: accessToken,
+            key: '6xURMARqiKBjGkKi0BQJkA((',
+            body: `Hi, @${username} your post was granted with tips, check it out on app.next-tips.com`
+        }), {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
             }
         })
 
